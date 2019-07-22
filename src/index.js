ab7357eaa056e8a5c892a5849703af3504f3026f
splean101 <101splean@gmail.com>
2019-07-22 16:45:22 +0300

    color2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 class CalcForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { value1: '',value1isValid: false, value2: '', value2isValid: false, res: 0 };
+        this.state = { value1: '', value1isValid: false, value2: '', value2isValid: false, res: 0 };
         this.onChange1 = this.onChange1.bind(this);
         this.onChange2 = this.onChange2.bind(this);
         this.add = this.add.bind(this);
@@ -12,7 +12,7 @@ class CalcForm extends React.Component {
         this.mul = this.mul.bind(this);
         this.div = this.div.bind(this);
     }
-    
+
     value1isValid(value1) {
         if (!isNaN(value1)) {
             return true;
@@ -26,22 +26,22 @@ class CalcForm extends React.Component {
         return false;
     }
     onChange1(event) {
-        this.setState({ value1: Number(event.target.value), value1isValid: this.value1isValid(Number(event.target.value))});
+        this.setState({ value1: Number(event.target.value), value1isValid: this.value1isValid(Number(event.target.value)) });
     }
     onChange2(event) {
-        this.setState({ value2: Number(event.target.value), value1isValid: this.value2isValid(Number(event.target.value))});
+        this.setState({ value2: Number(event.target.value), value1isValid: this.value2isValid(Number(event.target.value)) });
     }
-    add (){
-        return this.setState({res: this.state.value1 + this.state.value2});
+    add() {
+        return this.setState({ res: this.state.value1 + this.state.value2 });
     }
-    sub (){
-        return this.setState({res: this.state.value1 - this.state.value2});
+    sub() {
+        return this.setState({ res: this.state.value1 - this.state.value2 });
     }
-    mul (){
-        return this.setState({res: this.state.value1 * this.state.value2});
+    mul() {
+        return this.setState({ res: this.state.value1 * this.state.value2 });
     }
-    div (){
-        return this.setState({res: this.state.value1 / this.state.value2});
+    div() {
+        return this.setState({ res: this.state.value1 / this.state.value2 });
     }
     render() {
         let val1Color = this.state.value1isValid ? 'blue' : 'red';
@@ -49,10 +49,10 @@ class CalcForm extends React.Component {
         return (
             <form>
                 <label htmlFor="value1">value1:</label>
-                <input type="number" name="value1" value={this.state.value1} onChange={this.onChange1} style={{color: val1Color}}/>
+                <input type="number" name="value1" value={this.state.value1} onChange={this.onChange1} style={{ color: val1Color }} />
                 <br />
                 <label htmlFor="value2">value2:</label>
-                <input type="number" name="value2" value={this.state.value2} onChange={this.onChange2} style={{color: val2Color}}/>
+                <input type="number" name="value2" value={this.state.value2} onChange={this.onChange2} style={{ color: val2Color }} />
                 <br />
                 <button type="button" onClick={this.add}>+</button>
                 <button type="button" onClick={this.sub}>-</button>
