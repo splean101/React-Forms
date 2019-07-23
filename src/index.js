8be404c87be465ff28484ba999588913a626c12e
splean101 <101splean@gmail.com>
2019-07-23 08:04:02 +0300

    final

--- src/index.js
+++ src/index.js
@@ -5,14 +5,12 @@ class CalcForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = { value1: '', value1isValid: false, value2: '', value2isValid: false, res: 0 };
-        this.onChange1 = this.onChange1.bind(this);
-        this.onChange2 = this.onChange2.bind(this);
+        this.onChange = this.onChange.bind(this);
         this.add = this.add.bind(this);
         this.sub = this.sub.bind(this);
         this.mul = this.mul.bind(this);
         this.div = this.div.bind(this);
     }
-
     value1isValid(value1) {
         if (!isNaN(value1)) {
             return true;
@@ -25,11 +23,14 @@ class CalcForm extends React.Component {
         }
         return false;
     }
-    onChange1(event) {
-        this.setState({ value1: Number(event.target.value), value1isValid: this.value1isValid(Number(event.target.value)) });
-    }
-    onChange2(event) {
-        this.setState({ value2: Number(event.target.value), value1isValid: this.value2isValid(Number(event.target.value)) });
+    onChange(event) {
+        const{name, value} = event.target;
+        this.setState({ [name]: Number(value)});
+        if(name === 'value1'){
+            this.setState({value1isValid: this.value1isValid(Number(value))});
+        } else{
+            this.setState({ value2isValid: this.value2isValid(Number(value)) });
+        }       
     }
     add() {
         return this.setState({ res: this.state.value1 + this.state.value2 });
@@ -45,14 +46,14 @@ class CalcForm extends React.Component {
     }
     render() {
         let val1Color = this.state.value1isValid ? 'blue' : 'red';
-        let val2Color = this.state.value2isValid ? 'blue' : 'red';
+        let val2Color = this.state.value2isValid ? 'blue' : 'red';      
         return (
             <form>
                 <label htmlFor="value1">value1:</label>
-                <input type="number" name="value1" value={this.state.value1} onChange={this.onChange1} style={{ color: val1Color }} />
+                <input type="number" name="value1" value={this.state.value1} onChange={this.onChange} style={{ backgroundColor: val1Color }} />
                 <br />
                 <label htmlFor="value2">value2:</label>
-                <input type="number" name="value2" value={this.state.value2} onChange={this.onChange2} style={{ color: val2Color }} />
+                <input type="number" name="value2" value={this.state.value2} onChange={this.onChange} style={{ backgroundColor: val2Color }} />
                 <br />
                 <button type="button" onClick={this.add}>+</button>
                 <button type="button" onClick={this.sub}>-</button>
